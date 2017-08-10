396b45250794df1aa23925a96804ea78fba84038
Jituntpc <30657440+Jituntpc@users.noreply.github.com>
2017-08-10 11:50:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/arctile1', function (req, res) {
-  res.send('Sent arcticel one');
+  res.sendFile(path.join(__dirname,'ui','article1.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
